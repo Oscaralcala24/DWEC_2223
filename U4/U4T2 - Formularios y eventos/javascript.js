cff13a78df3916715309beca6bf74c0a5b0555e9
Oscaralcala24 <alum.oalcalae.2020@iesalixar.org>
2022-11-18 21:17:12 +0100

    juego

--- U4/U4T2 - Formularios y eventos/javascript.js
+++ U4/U4T2 - Formularios y eventos/javascript.js
@@ -1,3 +1,37 @@
-function comprobar(foto){
-    foto.classList.add('mostrar');
+var contadorImg = 0;
+let elementosTd = document.querySelectorAll("td");
+var arrayImg = [];
+
+function listener(event){
+    let objeto = event.currentTarget;
+    validar(objeto);
 }
+
+function validar(td){
+    arrayImg.push(td.firstChild);
+    td.firstChild.classList.add("mostrar");
+
+    if(arrayImg.length == 2){
+        
+        if(arrayImg[0].src == arrayImg[1].src){
+            if(arrayImg[0].id != arrayImg[1].id){
+                arrayImg[0].parentNode.classList.add("acertado");
+                arrayImg[1].parentNode.classList.add("acertado");
+                console.log(elementosTd)
+            }
+        }else{
+            
+            arrayImg[0].classList.remove("mostrar");
+            arrayImg[1].classList.remove("mostrar");
+        }
+        
+        arrayImg = [];
+        
+    }
+    
+}
+
+elementosTd.forEach(td=>{
+    
+    td.addEventListener("click", listener, false); 
+}) 
