b1f519583f5372ae1c5a1203339bad49eb97be8e
Oscaralcala24 <alum.oalcalae.2020@iesalixar.org>
2022-11-24 10:24:36 +0100

    validaciones

--- U4/U4T2 - Formularios y eventos/javascript.js
+++ U4/U4T2 - Formularios y eventos/javascript.js
@@ -1,37 +1,40 @@
+var contadorAux = 0;
 var contadorImg = 0;
 let elementosTd = document.querySelectorAll("td");
 var arrayImg = [];
-
+var objetoAnterior
 function listener(event){
     let objeto = event.currentTarget;
     validar(objeto);
 }
-
 function validar(td){
     arrayImg.push(td.firstChild);
     td.firstChild.classList.add("mostrar");
-
     if(arrayImg.length == 2){
-        
-        if(arrayImg[0].src == arrayImg[1].src){
-            if(arrayImg[0].id != arrayImg[1].id){
+        if(arrayImg[0].id != arrayImg[1].id){
+            if(arrayImg[0].src == arrayImg[1].src){
                 arrayImg[0].parentNode.classList.add("acertado");
                 arrayImg[1].parentNode.classList.add("acertado");
-                console.log(elementosTd)
+                console.log(elementosTd);
+                arrayImg[0].removeEventListener('click', listener);
+                arrayImg[1].removeEventListener('click', listener);
+                arrayImg = []; 
+                document.getElementById("numero").value = ++contadorAux;
+            }else{
+                setTimeout(borrarElemento,1000);
             }
         }else{
-            
-            arrayImg[0].classList.remove("mostrar");
-            arrayImg[1].classList.remove("mostrar");
+            setTimeout(borrarElemento,1000);
         }
-        
-        arrayImg = [];
-        
     }
-    
 }
 
 elementosTd.forEach(td=>{
-    
     td.addEventListener("click", listener, false); 
 }) 
+
+function borrarElemento(){
+    arrayImg[0].classList.remove("mostrar");
+    arrayImg[1].classList.remove("mostrar");
+    arrayImg = [];
+}
--- U4/U4T3 - Formularios - Validación básica/validacion.js
+++ U4/U4T3 - Formularios - Validación básica/validacion.js
@@ -1,7 +1,48 @@
-$FORM = document.forms[0];
-$NOMBREDISCO = document.getElementById("nombreDisco");
-$NOMBREGRUPO = document.getElementById("nombreGrupo");
-$ANOPUBLICACION = document.getElementById("anoPublicacion");
-$TIPOMUSICA = document.getElementById("tipoMusica");
-$LOCALIZACION = document.getElementById("estanteria");
-$PRESTADO = document.getElementById("prestado");
+window.onload = iniciarListener;
+
+function iniciarListener(){
+    $FORM = document.forms[0];
+    $NOMBREDISCO = document.getElementById("nombreDisco");
+    $NOMBREGRUPO = document.getElementById("nombreGrupo");
+    $ANOPUBLICACION = document.getElementById("anoPublicacion");
+    $TIPOMUSICA = document.getElementById("tipoMusica");
+    $LOCALIZACION = document.getElementById("estanteria");
+    $PRESTADO = document.getElementById("prestado");
+    
+    $FORM.addEventListener("submit", handleSubmit, false);
+}
+
+function handleSubmit(e){
+    nombreDisco = $NOMBREDISCO.value;
+    nombreGrupo = $NOMBREGRUPO.value;
+    anoPublicacion = $ANOPUBLICACION.value;
+    tipoMusica = $TIPOMUSICA.value;
+    localizacion = $LOCALIZACION.value;
+    prestado = $LOCALIZACION.value;
+    var valido = true;
+    debugger;
+    if(nombreDisco == false || isValidNombre(nombreDisco) == false){
+        alert("Nombre del disco incorrecto");
+        valido = false;
+    }
+    if(nombreGrupo == false || isValidNombre(nombreGrupo) == false){
+        alert("Nombre del grupo incorrecto");
+        valido = false;
+    }
+    if(anoPublicacion == false || isValidAnoPublicacion(anoPublicacion) == false){
+        alert("Año de publicacion incorrecto");
+        valido = false;
+    }
+}
+
+function isValidNombre(nombre){
+    console.log("Validacion nombre");
+    let validacion = /^[a-zA-Z]{1,20}$/;
+    return validacion.test(nombre);
+}
+
+function isValidAnoPublicacion(publicacion){
+    console.log("Validacion publicacion");
+    let validacion = /^[0-9]{4}$/;
+    return validacion.test(publicacion);
+}
